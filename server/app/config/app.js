6542e173526fc199a8d0799caf7cc4b6d1f2d4eb
Fortune Ekeruo <legalmody@gmail.com>
2017-10-04 13:24:19 +0100

    Modularize express app and make server easier to read

---
+++ server/app/config/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+import routes from './../routes/';
+
+const app = express();
+const router = express.Router();
+
+app.use(morgan('combined'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(router);
+
+// Fail safe for routes that don't exist
+router.get('*', (req, res) => {
+    res.send({ message: 'You may be lost! Go home'})
+})
+
+app.use('/api', router);
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,4 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-
-import routes from './app/routes/';
-
-const app = express();
-const router = express.Router();
-
-app.use(morgan('combined'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-routes(router);
-
-// Fail safe for routes that don't exist
-router.get('*', (req, res) => {
-    res.send({ message: 'You may be lost! Go home'})
-})
-
-app.use('/api', router);
+import app from './app/config/app';
 
 const port = process.env.PORT || 4040;
 app.listen(port);
